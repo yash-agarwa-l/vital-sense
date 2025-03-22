b9d973760affb889face2793965fd5d542404626
yash <yash162534@gmail.com>
2025-03-22 20:52:36 +0530

    Updated the model file and backend

---
+++ vital-sense/src/main/java/com/yash/vital_sense/VitalSenseApplication.java
@@ -0,0 +1,13 @@
+package com.yash.vital_sense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalSenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalSenseApplication.class, args);
+	}
+
+}
---
+++ vital-sense/src/test/java/com/yash/vital_sense/VitalSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.vital_sense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalSenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
